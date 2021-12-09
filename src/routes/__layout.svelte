4d48bd895d4b479e232bd98ef03fa79367e7eb17
Cahllagerfeld <Cahllagerfeld>
2021-12-09 10:15:18 +0000

    feat: init config

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
+	import fetch from 'unfetch';
+
 	import '../app.css';
+
+	onMount(async () => {
+		const configResponse = await fetch('/config.json');
+		const response = await configResponse.json();
+	});
 </script>
 
 <slot />
