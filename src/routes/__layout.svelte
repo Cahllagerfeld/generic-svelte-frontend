3c959a685a482c858625c5481bb16cff2c974b99
Cahllagerfeld <Cahllagerfeld>
2021-12-10 18:16:09 +0000

    feat: move fetch logic to load function

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,1 @@
-<script>
-	import { onMount } from 'svelte';
-	import fetch from 'unfetch';
-
-	import '../app.css';
-
-	onMount(async () => {
-		const configResponse = await fetch('/config.json');
-		const response = await configResponse.json();
-	});
-</script>
-
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,42 @@
-<script>
+<script lang="ts" context="module">
+	export async function load({ page, fetch, session, stuff }) {
+		const url = '/config/index.json';
+		const res = await fetch(url);
+
+		if (res.ok) {
+			const pageConfig = await res.json();
+			return {
+				props: {
+					pageConfig
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script lang="ts">
+	import type { config } from '../interfaces/config.interface';
+	import { onMount } from 'svelte';
+	export let pageConfig: config.Config;
 	import Card from '../components/card.svelte';
+	import fetch from 'unfetch';
+
+	let requestData = {};
+
+	onMount(async () => {
+		pageConfig.requests.forEach(async (request) => {
+			const res = await fetch(request.url, {
+				method: request.method
+			});
+			const config = await res.json();
+			requestData[request.key] = config;
+		});
+	});
 </script>
 
 <div class="grid grid-cols-3">
