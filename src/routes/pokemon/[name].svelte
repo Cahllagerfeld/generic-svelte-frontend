975ff4a71e8b57ab23accbf21b182f5a67e01640
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-12-13 12:20:58 +0100

    Cahllagerfeld/make card configurable 21 (#23)

--- src/routes/pokemon/[name].svelte
+++ src/routes/pokemon/[name].svelte
@@ -21,10 +21,33 @@
 <script lang="ts">
 	import type { config as configTypes } from '../../interfaces/config.interface';
 	import { onMount } from 'svelte';
+	import get from 'lodash.get';
+	import Card from '../../components/card.svelte';
 
 	export let routeConfig: configTypes.Config;
+	let requestData: any = {};
 
 	onMount(() => {
-		routeConfig.requests.forEach(async () => {});
+		routeConfig.requests.forEach(async (request) => {
+			const res = await fetch(request.url, {
+				method: request.method
+			});
+			const config = await res.json();
+			requestData[request.key] = config;
+		});
 	});
 </script>
+
+<div class="">
+	{#each Object.entries(routeConfig.tiles) as [_, config]}
+		{#if !config.isObject}
+			{#each Object.entries(get(requestData, config.binding, {})) as [_, dataset]}
+				<div class="mb-8">
+					<Card {config} data={dataset} />
+				</div>
+			{/each}
+		{:else}
+			<Card {config} data={get(requestData, config.binding, {})} />
+		{/if}
+	{/each}
+</div>
